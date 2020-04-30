cd8b77450fd870d00b4c1a687a2574ab1d3cc3af
Eloy <eloydgutierrez@gmail.com>
2020-04-30 19:13:47 -0400

    added removeItem Stretch task

--- src/App.js
+++ src/App.js
@@ -20,20 +20,23 @@ function App() {
 		return setCart([...cart, item])
 	};
 
+	const removeItem = itemRemoved => {
+		console.log("removing item", itemRemoved);
+		console.log("cart items", cart);
+		return setCart([...cart.filter(item => item.id !== itemRemoved.id)])
+	}
+
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-				<CartContext.Provider value={cart}>
+				<CartContext.Provider value={{cart, removeItem}}>
 					<Navigation />
 
 			{/* Routes */}
 			
-				
 					<Route exact path="/">
 						<Products />
 					</Route>
-				
-
 				
 					<Route path="/cart">
 						<ShoppingCart />
